81ada1909e76ba1483fde3b6826846b0631fb197
James Brodovsky <james.brodovsky@gmail.com>
2023-12-19 13:54:34 -0500

    Update .gitignore and updated basic testing functions documentation

--- test/test_angles.py
+++ test/test_angles.py
@@ -11,6 +11,9 @@ from src.anglewrapper.wrap import to_pi, to_2pi, to_180, to_360
 def test_to_pi():
     """
     Test wrap_to_pi function
+
+    This function tests the wrap_to_pi function by checking if the result of wrapping
+    the given radians to the range [-pi, pi) is equal to the expected value.
     """
     radians = 3 * math.pi / 2
     assert to_pi(radians) == -math.pi / 2
@@ -19,6 +22,9 @@ def test_to_pi():
 def test_to_2pi():
     """
     Test wrap_to_2pi function
+
+    This function tests the behavior of the wrap_to_2pi function by checking if the result of wrapping
+    the given radians to the range [0, 2pi) is equal to the expected value.
     """
     radians = 5 * math.pi / 2
     assert to_2pi(radians) == math.pi / 2
@@ -27,6 +33,9 @@ def test_to_2pi():
 def test_to_180():
     """
     Test wrap_to_180 function
+
+    This function tests the wrap_to_180 function by passing a degree value of 270.0
+    and asserting that the result is -90.0.
     """
     degrees = 270.0
     assert to_180(degrees) == -90.0
@@ -35,6 +44,9 @@ def test_to_180():
 def test_to_360():
     """
     Test wrap_to_360 function
+    
+    This function tests the behavior of the wrap_to_360 function by passing a value of 450.0 degrees
+    and asserting that the result is 90.0 degrees.
     """
     degrees = 450.0
     assert to_360(degrees) == 90.0
