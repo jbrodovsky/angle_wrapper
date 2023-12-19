e53ad925a9214fba763fc049b6de8f0c37c9e7ae
James Brodovsky <james.brodovsky@gmail.com>
2023-12-19 13:44:24 -0500

    formatted with black

--- src/anglewrapper/wrap.py
+++ src/anglewrapper/wrap.py
@@ -1,13 +1,17 @@
 import math
 
+
 def to_pi(radians):
     return (radians + math.pi) % (2 * math.pi) - math.pi
+
 
 def to_2pi(radians):
     return radians % (2 * math.pi)
 
+
 def to_180(degrees):
     return (degrees + 180) % 360 - 180
+
 
 def to_360(degrees):
     return degrees % 360
